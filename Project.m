1a35160ee17b35540c254fd6c8ffa990bd7d3050
AHKSASE2002 <21p0153@eng.asu.edu.eg>
2023-12-03 11:19:46 +0200

    update: project.m file and added frequency spectrum

--- Project.m
+++ Project.m
@@ -14,26 +14,48 @@ x = zeros(length(t), length(notes));
 % Create a variable to store individual energies
 energies = zeros(1, length(notes));
 
+% Create the combined signal
+combined_signal = [];
+
 for i = 1:4
     fn = f0 * (alpha ^ (n_values(i) / 12)); % Calculate the frequency
     x(:, i) = cos(2 * pi * fn * t); % Generate and store the signal in the matrix
     energies(i) = integral(@(t) (cos(2 * pi * fn * t)).^2, 0,0.5 ); % Calculate the energy for each note using the integral function
-end
-
-% Plot the signals
-for i = 1:4
+    
+    % Plot each signal
     subplot(2, 2, i);
     plot(t, x(:, i));
     title(['Signal for ' notes{i}]);
     xlabel('Time (s)');
     ylabel('Amplitude');
+    
 end
 
 % Play the signals sequentially
 combined_signal = reshape(x, 1, []); % Reshape the matrix to a row vector
-sound(combined_signal, fs);
+audiowrite('X.wav', combined_signal, fs);
 
 % Calculate the total energy of the combined signal
-energy_x = sum(energies);
+energy_x = sum(combined_signal.^2) / fs;
+disp(['Energy of the combined signal: ', num2str(energy_x)]);
 
-fprintf('Energy of the combined signal: %f\n', energy_x);
+% FFT and Spectrum Plot
+% Compute the FFT of the combined signal
+N = length(combined_signal); % Number of sample points
+fft_signal = fft(combined_signal);
+
+% Compute the two-sided spectrum and then convert to a one-sided spectrum
+two_sided_spectrum = abs(fft_signal / N);
+one_sided_spectrum = two_sided_spectrum(1:N/2+1);
+f = fs * (0:(N/2)) / N;
+
+% Plot the frequency spectrum
+figure;
+plot(f, one_sided_spectrum);
+title('Frequency Spectrum of the Combined Signal');
+xlabel('Frequency (Hz)');
+ylabel('Magnitude');
+
+% Calculate the energy of the FFT-transformed signal
+energy_fft = sum(abs(fft_signal).^2) / (2 * fs) / fs;
+disp(['Energy of the FFT-transformed signal: ', num2str(energy_fft)]);
